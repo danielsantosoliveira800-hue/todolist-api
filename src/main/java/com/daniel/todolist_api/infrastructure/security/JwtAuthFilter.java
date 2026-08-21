28d02a55cb10d409b5bc2f01e39e4732b9fce44d
danielsantosoliveira800-hue <danielsantosoliveira800@gmail.com>
2026-08-20 21:24:18 -0300

    feat: adiciona SecurityFilterChain e conclui configuração de segurança JWT

--- src/main/java/com/daniel/todolist_api/infrastructure/security/JwtAuthFilter.java
+++ src/main/java/com/daniel/todolist_api/infrastructure/security/JwtAuthFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -12,10 +13,14 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+
+@Component
+@RequiredArgsConstructor
 public class JwtAuthFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
